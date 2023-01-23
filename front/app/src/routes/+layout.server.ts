1106df5aad9871152ed596e01319563bb78173e3
Ramiro <ramiroaisen@gmail.com>
2023-01-23 18:19:47 -0300

    fix: config key load

--- front/app/src/routes/+layout.server.ts
+++ front/app/src/routes/+layout.server.ts
@@ -6,6 +6,6 @@ export const ssr = !dev;
 export const load = (async ({ request, getClientAddress, depends }) => {
   depends("user:me");
   const maybe_user = await load_get_me({ request, getClientAddress });
-  const config: import("$server/config").Config["public"] = await load_get("/api/config", { request, getClientAddress });
+  const { config }: { config: import("$server/config").Config["public"] } = await load_get("/api/config", { request, getClientAddress });
   return { config, maybe_user };
 }) satisfies import("./$types").LayoutServerLoad;
