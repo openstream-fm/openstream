f76fe1340c5b6d47b961492bfc40d12de3399687
Ramiro <ramiroaisen@gmail.com>
2023-04-07 23:04:50 -0300

    env color pm2 config

--- front/server/pm2.config.cjs
+++ front/server/pm2.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
     args: ["start"],
     interpreter_args: ["--no-warnings", "--experimental-specifier-resolution=node"],
     name: "openstream-front",
+    env: { FORCE_COLOR: 1 },
     instances: 2,
     mode: "cluster",
   }
