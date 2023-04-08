8e0fca865f7711551e9a99186864f57b0bcb3748
Ramiro <ramiroaisen@gmail.com>
2023-04-07 23:03:38 -0300

    fix openstream front pm2 config

--- front/server/pm2.config.cjs
+++ front/server/pm2.config.cjs
@@ -2,8 +2,9 @@ module.exports = {
   apps: {
     script: "./openstream-front.js",
     args: ["start"],
+    interpreter_args: ["--no-warnings", "--experimental-specifier-resolution=node"],
     name: "openstream-front",
-    instances: 12,
+    instances: 2,
     mode: "cluster",
   }
 }
