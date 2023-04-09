fb2276f7b8fb7d2ee4e3431f2429f59b17b540ad
Ramiro <ramiroaisen@gmail.com>
2023-04-09 01:47:48 -0300

    change to signed cookies

--- front/server/src/session.ts
+++ front/server/src/session.ts
@@ -11,51 +11,51 @@ export type SessionData = {
   user: { _id: string, token: string, media_key: string } | null;
 }
 
-const ALGO = "aes-192-cbc"
+// const ALGO = "aes-192-cbc"
 
-export function encrypt(value: string, key: Buffer, logger: Logger): string {
-  try {
-    const iv = crypto.randomBytes(16);
-    const cipher = crypto.createCipheriv(ALGO, key, iv);
-    const hash = cipher.update(value, "utf8", "base64") + cipher.final("base64");
-    const encrypted = `${iv.toString("hex")}.${hash}`;
-    // logger.info(`ecrypted: ${encrypted}`)
-    return encrypted;
-  } catch(e) {
-    logger.warn(`encrypt error: ${e}`)
-    throw e;
-  }
-}
+// export function encrypt(value: string, key: Buffer, logger: Logger): string {
+//   try {
+//     const iv = crypto.randomBytes(16);
+//     const cipher = crypto.createCipheriv(ALGO, key, iv);
+//     const hash = cipher.update(value, "utf8", "base64") + cipher.final("base64");
+//     const encrypted = `${iv.toString("hex")}.${hash}`;
+//     // logger.info(`ecrypted: ${encrypted}`)
+//     return encrypted;
+//   } catch(e) {
+//     logger.warn(`encrypt error: ${e}`)
+//     throw e;
+//   }
+// }
 
-export function decrypt(hash: string, key: Buffer, logger: Logger): string {
-  try {
-    const [ivhex, base64] = hash.split(".");
-    if(!ivhex || !base64) throw new Error("malformed hash: iv or hash missing");
-    const iv = Buffer.from(ivhex, "hex");
-    const decipher = crypto.createDecipheriv(ALGO, key, iv);
-    const value = decipher.update(base64, "base64", "utf8") + decipher.final("utf8");
-    // logger.info(`decrypted: ${value}`);
-    return value;
-  } catch(e) {
-    logger.warn(`decrypt error: ${e}`)
-    throw e;
-  }
-}
+// export function decrypt(hash: string, key: Buffer, logger: Logger): string {
+//   try {
+//     const [ivhex, base64] = hash.split(".");
+//     if(!ivhex || !base64) throw new Error("malformed hash: iv or hash missing");
+//     const iv = Buffer.from(ivhex, "hex");
+//     const decipher = crypto.createDecipheriv(ALGO, key, iv);
+//     const value = decipher.update(base64, "base64", "utf8") + decipher.final("utf8");
+//     // logger.info(`decrypted: ${value}`);
+//     return value;
+//   } catch(e) {
+//     logger.warn(`decrypt error: ${e}`)
+//     throw e;
+//   }
+// }
 
-const get_cookie_session = (req: Request, key: Buffer, logger: Logger): SessionData => {
+const get_cookie_session = (req: Request, logger: Logger): SessionData => {
   try {
-    const v = req.cookies[COOKIE_NAME];
+    const value = req.signedCookies[COOKIE_NAME];
     // logger.info(`v: ${v}`)
-    if(typeof v !== "string") {
+    if(typeof value !== "string") {
       // logger.info(`not string, ${typeof v}`)
       return { user: null };
     }
-    const json_string = decrypt(v, key, logger);
+    // const json_string = decrypt(v, key, logger);
     let data: any;
     try {
-      data = JSON.parse(json_string);
+      data = JSON.parse(value);
     } catch (e) {
-      logger.warn(`json parse error: JSON.parse('${json_string}'): ${e}`)
+      logger.warn(`json parse error: JSON.parse('${value}'): ${e}`)
     }
 
     if(is<SessionData>(data)) {
@@ -85,20 +85,20 @@ declare global {
 
 export const session = (config: Config, _logger: Logger) => {
   const logger = _logger.scoped("cookie-session");
-  const key = crypto.scryptSync(config.session.secret, "salt", 24);
+  // const key = crypto.scryptSync(config.session.secret, "salt", 24);
 
   const name = config.session.cookieName || COOKIE_NAME;
   const maxAge = config.session.maxAgeDays * 1000 * 60 * 60 * 24;
-  const options = { maxAge, httpOnly: true, sameSite: "strict" as const };
-  const clear_options = { httpOnly: options.httpOnly, sameSite: options.sameSite };
+  const options = { maxAge, httpOnly: true,  sameSite: "strict" as const, signed: true, domain: config.session.domain };
+  const clear_options = { httpOnly: options.httpOnly, sameSite: options.sameSite, domain: options.domain, signed: true };
 
   const router = Router();
-  router.use(cookieParser());
+  router.use(cookieParser(config.session.secret));
   router.use((req: Request, res: Response, next: NextFunction) => {
-    req.cookie_session = get_cookie_session(req, key, logger);
+    req.cookie_session = get_cookie_session(req, logger);
     res.set_session = (data: SessionData) => {
-      const encoded = encrypt(JSON.stringify(data), key, logger);
-      res.cookie(name, encoded, options);
+      //const encoded = encrypt(JSON.stringify(data), key, logger);
+      res.cookie(name, JSON.stringify(data), options);
     }
     res.clear_session = () => res.clearCookie(name, clear_options);
     next();
