36dfd612a46a4bbba5fa2c4738c744652af74d3a
Ramiro <ramiroaisen@gmail.com>
2023-04-12 00:43:28 -0300

    fixing m3u

--- packages/stream/src/lib.rs
+++ packages/stream/src/lib.rs
@@ -609,7 +609,7 @@ pub struct HlsContents<'a> {
 
 impl<'a> Display for HlsContents<'a> {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-      writeln!(f, "#EXTM3U")?;
+      // writeln!(f, "#EXTM3U")?;
       writeln!(f, "{}", self.target)?;
       Ok(())
   }
