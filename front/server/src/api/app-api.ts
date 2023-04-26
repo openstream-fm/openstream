5f1b339bb2e3bc15a3466384917370a4ddedacc3
Ramiro <ramiroaisen@gmail.com>
2023-04-26 09:28:52 -0300

    fix front s1 s2 ports

--- front/server/src/api/app-api.ts
+++ front/server/src/api/app-api.ts
@@ -27,10 +27,10 @@ export const public_config = (host: string, source_port_map: Config["source_port
     port = source_port_map.default;
   } else if (host === "studio.test.openstream.fm") {
     port = source_port_map.test;
-  } else if (host === "studio.srv1.openstream.fm") {
-    port = source_port_map.srv1;
-  } else if (host === "studio.srv2.openstream.fm") {
-    port = source_port_map.srv2;
+  } else if (host === "studio.s1.openstream.fm") {
+    port = source_port_map.s1;
+  } else if (host === "studio.s2.openstream.fm") {
+    port = source_port_map.s2;
   } else if (host === "studio.local.openstream.fm") {
     port = source_port_map.local;
   } else {
