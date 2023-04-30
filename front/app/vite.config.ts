1c053ad10b1fb8d0463f4276065828cfd68a6262
Ramiro <ramiroaisen@gmail.com>
2023-04-30 16:57:03 -0300

    remove unused js deps

--- front/app/vite.config.ts
+++ front/app/vite.config.ts
@@ -3,7 +3,7 @@ import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
 	resolve: {
-		dedupe: ["@sveltejs/kit", "svelte", "@mdi/js", "http-status-codes"],
+		dedupe: ["@sveltejs/kit", "svelte", "@mdi/js", "http-status-codes", "kleur"],
 	},
 	server: {
 		https: false,
