f07271337ebc9c3f8601c2c1609c7668aeac480e
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2023-06-23 20:23:54 -0300

    fix: remove byte debug

--- rs/packages/source-alt/src/http/mod.rs
+++ rs/packages/source-alt/src/http/mod.rs
@@ -86,7 +86,6 @@ pub async fn read_request_head<R: AsyncRead + Unpin>(
     }
 
     let byte = reader.read_u8().await?;
-    info!("byte: {:?}", String::from_utf8_lossy(&[byte]));
 
     if byte == b'\n'
       && ((i >= 3 && &buf[(i - 3)..i] == b"\r\n\r") || (i >= 1 && buf[i - 1] == b'\n'))
