0103e3068241e7862781292efa93ca166c73bef8
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2023-06-24 20:45:52 -0300

    feat: add admin stats map to admin dashboard

--- front/admin/src/routes/(online)/(app)/+page.ts
+++ front/admin/src/routes/(online)/(app)/+page.ts
@@ -1,3 +1,9 @@
-export const load = (async () => {
-  return {};
+import { load_get } from "$lib/load";
+
+export const load = (async ({ fetch, url }) => {
+  
+  const { stats } = await load_get<import("$server/defs/api/stream-stats/GET/Output").Output>("/api/stream-stats", { fetch, url });
+
+  return { stats }
+
 }) satisfies import("./$types").PageLoad;
