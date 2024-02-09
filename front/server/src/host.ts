938e6c3e1ca7c3bf205ccee38666a44a998abeab
ramiroaisen <52116153+ramiroaisen@users.noreply.github.com>
2024-02-08 22:55:14 -0300

    fix: forward sveltekit to api host as x-host

--- front/server/src/host.ts
+++ front/server/src/host.ts
@@ -2,7 +2,7 @@ import type { Request } from "express";
 import type { Config, HostConfig } from "./config.js";
 
 export const host = (mode: "studio" | "site" | "admin", config: Config["hosts"], req: Request): HostConfig & { id: string } => {
-  const host = req.hostname;
+  let host = req.headers["x-host"] || req.hostname;
   
   const { default: root, ...other } = config;
 
